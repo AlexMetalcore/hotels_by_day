ac253d14ac318f87db681bc654d408085162bfff
AlexMetalcore <alexandrov141089@gmail.com>
2021-10-16 23:20:25 +0300

    hotels by day

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/create_example_data.php';
 
 use App\Mankind;
-use App\ParseFile;
 use App\ParseLargeFile;
 use App\Person;
+use App\ParseFile;
 
 $file = __DIR__ . '/randomPersons.csv';
 
@@ -18,10 +18,10 @@ try {
 
     if ($filesize <= pow(10000, 2)) {
         $mankind = Mankind::getInstance(new ParseFile());
-        echo 'Small size file' . '<br>';
+        echo 'Small size file' . PHP_EOL;
     } else {
         $mankind = Mankind::getInstance(new ParseLargeFile());
-        echo 'Large size file' . '<br>';
+        echo 'Large size file' . PHP_EOL;
     }
 
     $data = $mankind->getPersons($file);
@@ -29,21 +29,17 @@ try {
     foreach ($data as $item) {
         /* @var Person $item */
         $person = $mankind->getPersonById($item->getId());
-        echo 'Get person age in days - ' . $person->getPersonAgeDays() . '<br>';
-        echo 'Get the Person based on ID: id - ' . $person->getId() . '; name - ' . $person->getName() . '; surname - ' . $person->getSurname() . '; gender - ' . $person->getSex() . '<br>';
+        echo 'Get person age in days - ' . $person->getPersonAgeDays() . PHP_EOL;
+        echo 'Get the Person based on ID: ' . $person->getName() . '; name - ' . $person->getName() . '; surname - ' . $person->getSurname() . '; gender - ' . $person->getSex() . PHP_EOL;
     }
 
-    echo 'Get the percentage of Men in Mankind - ' . $mankind->getPercentageOfMenInMankind() . '<br>';
+    echo 'Get the percentage of Men in Mankind - ' . $mankind->getPercentageOfMenInMankind() . PHP_EOL;
 
-    $mem_usage = memory_get_usage();
-    $mem_peak = memory_get_peak_usage();
-    $time = microtime(true) - $start;
+    echo 'Time usage ' . round(microtime(true) - $start, 2) . ' s' . PHP_EOL;
+    echo 'Memory usage ' . round(memory_get_usage() / (1024 * 1024)) . ' Mb' . PHP_EOL;
+    echo 'Max memory usage ' . round(memory_get_peak_usage() / (1024 * 1024)) . ' Mb' . PHP_EOL;
 
-    echo 'Time usage ' . round($time, 2) . ' s' . '<br>';
-    echo 'Memory usage ' . round($mem_usage / (1024 * 1024)) . ' Mb' . '<br>';
-    echo 'Max memory usage ' . round($mem_peak / (1024 * 1024)) . ' Mb' . '<br>';
-
-    echo 'File size - ' . round($filesize / (1024 * 1024)) . ' Mb' . '<br>';
+    echo 'File size - ' . round($filesize / (1024 * 1024)) . ' Mb' . PHP_EOL;
 
 } catch (Exception $e) {
     echo $e->getMessage() . PHP_EOL;
